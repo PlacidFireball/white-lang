ae428c348d2980176434a45ec4bb06558fe2704d
Jared Weiss <jared.lee.weiss@gmail.com>
2022-03-21 13:53:33 -0600

    add expression.rs and statement.rs

---
+++ src/parser/expression.rs
@@ -0,0 +1,16 @@
+// Expression modules
+pub(crate) mod additiveexpression;
+pub(crate) mod booleanliteralexpression;
+pub(crate) mod equalityexpression;
+pub(crate) mod comparisonexpression;
+pub(crate) mod factorexpression;
+pub(crate) mod floatliteralexpression;
+pub(crate) mod functioncallexpression;
+pub(crate) mod identifierexpression;
+pub(crate) mod integerliteralexpression;
+pub(crate) mod listliteralexpression;
+pub(crate) mod nullliteralexpression;
+pub(crate) mod parenthesizedexpression;
+pub(crate) mod stringliteralexpression;
+pub(crate) mod syntaxerrorexpression;
+pub(crate) mod unaryexpression;
---
+++ src/parser/statement.rs
@@ -0,0 +1,10 @@
+pub(crate) mod assignmentstatement;
+// statements
+pub(crate) mod forstatement;
+pub(crate) mod functioncallstatement;
+pub(crate) mod functiondefinitionstatement;
+pub(crate) mod printstatement;
+pub mod ifstatement;
+pub(crate) mod returnstatement;
+pub(crate) mod syntaxerrorstatement;
+pub(crate) mod variablestatement;
