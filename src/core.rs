1fb25b6e0b32ea7ce196844e9fcbb83f76ab7d2a
Jared Weiss <jared.lee.weiss@gmail.com>
2022-07-10 17:34:18 -0600

    add CORE, make break statement kinda work, other stuff

---
+++ src/core.rs
@@ -0,0 +1,37 @@
+
+use crate::tokenizer::Tokenizer;
+use crate::parser::Parser;
+use crate::runtime::Runtime;
+use crate::program::Program;
+
+pub struct Core {
+    pub parser: Parser,
+    pub tokenizer: Tokenizer,
+    pub program: Program
+}
+impl Core {
+    pub(crate) fn new(src: &str) -> Self {
+        let mut tokenizer = Tokenizer::init(src.to_string());
+        tokenizer.tokenize();
+        let mut parser = Parser::new(&mut tokenizer);
+        parser.parse();
+        let mut program = Program::from_parser(&mut parser);
+        Self {
+            parser,
+            tokenizer,
+            program
+        }
+    }
+
+    pub fn get_tokenizer(&mut self) -> &mut Tokenizer {
+        &mut self.tokenizer
+    }
+
+    pub fn get_parser(&mut self) -> &mut Parser {
+        &mut self.parser
+    }
+
+    pub fn get_program(&mut self) -> &mut Program {
+        &mut self.program
+    }
+}
